f64c9f6ca5c746c4e5d2f02b93e7edd17edd0a82
derrysukrie <derrysukrie@gmail.com>
2025-01-07 21:47:46 +0700

    ✨ (app.json): add package name and runtime version policy for better app configuration ✨ (detail.tsx): create a new DetailPage component to encapsulate detail logic and improve code organization 🐛 (SearchBar.tsx): add search functionality to log search terms with a delay ✨ (home.tsx): implement deep linking to navigate to detail page based on URL parameters 🔧 (eas.json): configure local build settings for internal distribution and development client ⬆️ (package.json): upgrade expo-updates dependency to the latest version for improved functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,17 @@
+import { useCallback } from "react";
 import { TextInput, TextInputProps, View } from "react-native";
 
+
 export const SearchBar = (props?: TextInputProps) => {
+  const handleSearch = useCallback((text: string) => {
+    setTimeout(() => {
+      console.log('Search Term:', text);
+    }, 1400);
+  }, []);
+
   return (
-    <View
-      style={{ marginVertical: 20, height: 50, display: "flex", paddingHorizontal: 10, justifyContent: "center", borderRadius: 10, backgroundColor: "#f6f6f6" }}
-    >
-      <TextInput placeholder="Search place..." style={{ fontSize: 16 }} placeholderTextColor="#666" {...props} />
+    <View style={{ marginVertical: 20, height: 50, display: "flex", paddingHorizontal: 10, justifyContent: "center", borderRadius: 10, backgroundColor: "#f6f6f6" }}>
+      <TextInput onChangeText={handleSearch} placeholder="Search place..." style={{ fontSize: 16 }} placeholderTextColor="#666" {...props} />
     </View>
   );
 };
--- src/screen/home/home.tsx
+++ src/screen/home/home.tsx
@@ -3,14 +3,45 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Places } from "./components/places";
+import { useContext, useEffect } from "react";
+import { Linking } from "react-native";
+import { useRouter } from "expo-router";
+import { HistoricalPlacesContext } from "@/src/context/AppContext";
 
 export default function HomeScreen() {
+  const router = useRouter();
+  const { places, toggleVisited } = useContext(HistoricalPlacesContext);
+
+  useEffect(() => {
+    const handleDeepLink = (event: any) => {
+      const url = event.url;
+      const params = new URLSearchParams(url.split("?")[1]);
+      const id = params.get("id") as string;
+
+      if (params.has("id")) {
+        toggleVisited(places[Number(id)].id);
+
+        router.push({
+          pathname: "/detail/[id]",
+          params: { id, data: String(id) },
+        });
+      }
+    };
+
+    Linking.addEventListener("url", handleDeepLink);
+
+    return () => {
+      Linking.removeAllListeners("url");
+    };
+  }, []);
+
   return (
     <SafeAreaView>
       <ThemedView style={{ paddingHorizontal: 20 }}>
-        <ThemedText type="title">Explore</ThemedText>
-        <ThemedText type="title">all sights</ThemedText>
-        <SearchBar readOnly />
+        <ThemedView style={{ paddingVertical: 20 }}>
+          <ThemedText type="title">Explore</ThemedText>
+          <ThemedText type="title">all sights</ThemedText>
+        </ThemedView>
         <Places />
       </ThemedView>
     </SafeAreaView>
